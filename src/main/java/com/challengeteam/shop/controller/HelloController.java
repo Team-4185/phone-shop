dabfb182ea94adbf9f58d442391c8c590be420f0
BohdanYarovyi <bogdan.yarovoy.01@gmail.com>
2025-09-16 21:27:20 +0300

    Initial commit

---
+++ src/main/java/com/challengeteam/shop/controller/HelloController.java
@@ -0,0 +1,38 @@
+package com.challengeteam.shop.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> getHello() {
+        String pingLink = "/ping";
+        String template = """
+                {
+                    "greeting" : "Hello world!",
+                    "ping-link" : "%s"
+                }
+                """;
+        String response = template.formatted(pingLink);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        String response = """
+                {
+                    "status" : "OK"
+                }
+                """;
+
+        return ResponseEntity.ok(response);
+    }
+
+
+}
