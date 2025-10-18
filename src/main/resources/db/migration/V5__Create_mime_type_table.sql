5cfcb83ae4bc867aa7ded06b874c05893dff8241
BohdanYarovyi <bogdan.yarovoy.01@gmail.com>
2025-10-18 14:20:04 +0300

    fix: Add UNIQUE constraint to 'extension' field.

--- src/main/resources/db/migration/V5__Create_mime_type_table.sql
+++ src/main/resources/db/migration/V5__Create_mime_type_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE mime_types
 (
     id        BIGINT GENERATED ALWAYS AS IDENTITY,
-    extension VARCHAR NOT NULL,
-    type      VARCHAR NOT NULL,
+    extension VARCHAR(32)  NOT NULL UNIQUE,
+    type      VARCHAR(128) NOT NULL,
     CONSTRAINT PK_mime_types_id PRIMARY KEY (id)
 );
