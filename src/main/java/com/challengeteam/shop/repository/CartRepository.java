3c50a45a7be2de819d33c467eb9114c234ee9e32
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-10-24 12:19:05 +0300

    Add CartRepository

---
+++ src/main/java/com/challengeteam/shop/repository/CartRepository.java
@@ -0,0 +1,14 @@
+package com.challengeteam.shop.repository;
+
+import com.challengeteam.shop.entity.cart.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByUserId(Long userId);
+
+}
