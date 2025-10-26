e35e4a4e68e6c67b1f309b4d97f8ed274a714bd9
BohdanYarovyi <bogdan.yarovoy.01@gmail.com>
2025-10-26 03:33:10 +0200

    fix: Create bucket if exists when upload new image.

--- src/main/java/com/challengeteam/shop/persistence/storage/MinioImageStorage.java
+++ src/main/java/com/challengeteam/shop/persistence/storage/MinioImageStorage.java
@@ -2,12 +2,19 @@ package com.challengeteam.shop.persistence.storage;
 
 import com.challengeteam.shop.exceptionHandling.exception.ImageStorageException;
 import com.challengeteam.shop.properties.MinioProperties;
+import io.minio.BucketExistsArgs;
 import io.minio.GetObjectArgs;
 import io.minio.GetObjectResponse;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import io.minio.RemoveObjectArgs;
 import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InsufficientDataException;
+import io.minio.errors.InternalException;
+import io.minio.errors.InvalidResponseException;
+import io.minio.errors.ServerException;
+import io.minio.errors.XmlParserException;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +22,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -31,7 +41,7 @@ public class MinioImageStorage implements ImageStorage {
 
 
     @PostConstruct
-    public void init() {
+    public void init() throws ImageStorageException {
         var url = minioProperties.getUrl();
         var username = minioProperties.getUsername();
         var password = minioProperties.getPassword();
@@ -42,6 +52,36 @@ public class MinioImageStorage implements ImageStorage {
                 .credentials(username, password)
                 .build();
         log.info("MinioImageStorage successfully initiated");
+
+        initBucketIfAbsent(IMAGE_BUCKET_NAME);
+    }
+
+    private void initBucketIfAbsent(String bucketName) throws ImageStorageException {
+        if (!bucketExists(bucketName)) {
+            var args = MakeBucketArgs.builder()
+                    .bucket(bucketName)
+                    .build();
+            try {
+                minioClient.makeBucket(args);
+            } catch (Exception e) {
+                throw new ImageStorageException("Failed to create bucket with name: " + bucketName);
+            }
+            log.info("Bucket with name {} successfully initiated", bucketName);
+        } else {
+            log.info("Bucket with name {} already exists", bucketName);
+        }
+    }
+
+    private boolean bucketExists(String bucketName) throws ImageStorageException {
+        var args = BucketExistsArgs.builder()
+                .bucket(bucketName)
+                .build();
+
+        try {
+            return minioClient.bucketExists(args);
+        } catch (Exception e) {
+            throw new ImageStorageException("Failed to check bucket exists by name: " + bucketName);
+        }
     }
 
     @PreDestroy
