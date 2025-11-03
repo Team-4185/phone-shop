68ec00a388aae767c30bf20cda2795b0d3ddf94a
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-11-03 18:52:38 +0200

    refactor .sql scripts

--- src/main/resources/db/migration/V8__Create_carts_items_table.sql
+++ src/main/resources/db/migration/V8__Create_carts_items_table.sql
@@ -4,15 +4,12 @@ CREATE TABLE carts_items
     fk_cart_id  BIGINT  NOT NULL,
     fk_phone_id BIGINT  NOT NULL,
     amount      INTEGER NOT NULL,
-
     CONSTRAINT PK_carts_items_id PRIMARY KEY (id),
-
-    CONSTRAINT fk_carts_items_cart
+    CONSTRAINT FK_carts_items_fk_cart_id
         FOREIGN KEY (fk_cart_id)
             REFERENCES carts (id)
             ON DELETE CASCADE,
-
-    CONSTRAINT fk_carts_items_phone
+    CONSTRAINT FK_carts_items_fk_phone_id
         FOREIGN KEY (fk_phone_id)
             REFERENCES phones (id)
             ON DELETE CASCADE
