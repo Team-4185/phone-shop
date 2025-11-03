499dbc30a24ad064b5f44d4fe78213b6f755887b
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-11-03 18:45:15 +0200

    add CartItemMapper

---
+++ src/main/java/com/challengeteam/shop/mapper/CartItemMapper.java
@@ -0,0 +1,15 @@
+package com.challengeteam.shop.mapper;
+
+import com.challengeteam.shop.dto.cart.CartItemResponseDto;
+import com.challengeteam.shop.entity.cart.CartItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CartItemMapper {
+
+    @Mapping(target = "cartId", source = "cart.id")
+    @Mapping(target = "phoneId", source = "phone.id")
+    CartItemResponseDto toCartItemResponseDto(CartItem cartItem);
+
+}
