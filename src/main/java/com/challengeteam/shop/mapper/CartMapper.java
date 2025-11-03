475f421e29f5378402d87e0cf948d0d65afd4b1a
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-11-03 18:47:54 +0200

    refactor: fix cartItems mapping

--- src/main/java/com/challengeteam/shop/mapper/CartMapper.java
+++ src/main/java/com/challengeteam/shop/mapper/CartMapper.java
@@ -3,10 +3,12 @@ package com.challengeteam.shop.mapper;
 import com.challengeteam.shop.dto.cart.CartResponseDto;
 import com.challengeteam.shop.entity.cart.Cart;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {CartItemMapper.class})
 public interface CartMapper {
 
+    @Mapping(target = "cartItems", source = "cartItems")
     CartResponseDto toCartResponseDto(Cart cart);
 
 }
