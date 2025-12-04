0b9df6a262401d9476961ec5e6e3de1071955ac1
BohdanYarovyi <bogdan.yarovoy.01@gmail.com>
2025-12-04 14:50:01 +0200

    fix: Validate input objects in AuthController.

--- src/main/java/com/challengeteam/shop/web/controller/AuthController.java
+++ src/main/java/com/challengeteam/shop/web/controller/AuthController.java
@@ -1,11 +1,12 @@
 package com.challengeteam.shop.web.controller;
 
 import com.challengeteam.shop.dto.jwt.JwtRefreshRequestDto;
-import com.challengeteam.shop.dto.user.UserLoginRequestDto;
 import com.challengeteam.shop.dto.jwt.JwtResponseDto;
+import com.challengeteam.shop.dto.user.UserLoginRequestDto;
 import com.challengeteam.shop.dto.user.UserRegisterRequestDto;
 import com.challengeteam.shop.service.JwtAuthorizationService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +26,7 @@ public class AuthController {
             description = "Gets user's credentials and registers user with unique email. Returns JWT pair."
     )
     @PostMapping("/register")
-    public ResponseEntity<JwtResponseDto> register(@RequestBody UserRegisterRequestDto userRegisterRequestDto) {
+    public ResponseEntity<JwtResponseDto> register(@RequestBody @Valid UserRegisterRequestDto userRegisterRequestDto) {
         JwtResponseDto jwtResponseDto = jwtAuthorizationService.register(userRegisterRequestDto);
 
         return ResponseEntity.ok(jwtResponseDto);
@@ -36,7 +37,7 @@ public class AuthController {
             description = "If the user registered in the system, gets user credentials for login. Returns JWT pair."
     )
     @PostMapping("/login")
-    public ResponseEntity<JwtResponseDto> login(@RequestBody UserLoginRequestDto userLoginRequestDto) {
+    public ResponseEntity<JwtResponseDto> login(@RequestBody @Valid UserLoginRequestDto userLoginRequestDto) {
         JwtResponseDto jwtResponseDto = jwtAuthorizationService.login(userLoginRequestDto);
 
         return ResponseEntity.ok(jwtResponseDto);
@@ -47,7 +48,7 @@ public class AuthController {
             description = "Takes 'refreshToken' and if token valid, creates a new token pair. Returns JWT pair."
     )
     @PostMapping("/refresh-token")
-    public ResponseEntity<JwtResponseDto> refreshToken(@RequestBody JwtRefreshRequestDto jwtRefreshRequestDto) {
+    public ResponseEntity<JwtResponseDto> refreshToken(@RequestBody @Valid JwtRefreshRequestDto jwtRefreshRequestDto) {
         String refreshToken = jwtRefreshRequestDto.refreshToken();
         JwtResponseDto jwtResponseDto = jwtAuthorizationService.refresh(refreshToken);
 
