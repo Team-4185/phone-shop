42314c2c2e804d017a3882246f517d6a1859a7b3
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-12-12 19:49:37 +0200

    fix: remove @NotBlank annotation

--- src/main/java/com/challengeteam/shop/dto/user/UpdateProfileDto.java
+++ src/main/java/com/challengeteam/shop/dto/user/UpdateProfileDto.java
@@ -1,12 +1,10 @@
 package com.challengeteam.shop.dto.user;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UpdateProfileDto(
 
-        @NotBlank(message = "First name must not be empty")
         @Size(min = 3, max = 255, message = "First name must be between {min} and {max} characters")
         @Pattern(
                 regexp = "^[A-Za-zА-Яа-яІіЇїЄєҐґ]+$",
@@ -14,7 +12,6 @@ public record UpdateProfileDto(
         )
         String newFirstname,
 
-        @NotBlank(message = "Last name must not be empty")
         @Size(min = 3, max = 255, message = "Last name must be between {min} and {max} characters")
         @Pattern(
                 regexp = "^[A-Za-zА-Яа-яІіЇїЄєҐґ]+$",
@@ -22,7 +19,6 @@ public record UpdateProfileDto(
         )
         String newLastname,
 
-        @NotBlank(message = "City must not be empty")
         @Size(min = 3, max = 255, message = "City must be between {min} and {max} characters")
         @Pattern(
                 regexp = "^[A-Za-zА-Яа-яІіЇїЄєҐґ]+$",
@@ -30,7 +26,6 @@ public record UpdateProfileDto(
         )
         String newCity,
 
-        @NotBlank(message = "Phone number must not be empty")
         @Pattern(
                 regexp = "^(\\+?380)([0-9]{9})$",
                 message = "Phone number must be in the format +380XXXXXXXXX"
