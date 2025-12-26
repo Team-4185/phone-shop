1a64dcd9dd151098dd3f1b12586f6932a9ef467d
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-12-26 21:52:27 +0200

    fix: add validation in UserCartController

--- src/main/java/com/challengeteam/shop/dto/cart/CartItemRemoveRequestDto.java
+++ src/main/java/com/challengeteam/shop/dto/cart/CartItemRemoveRequestDto.java
@@ -1,7 +1,15 @@
 package com.challengeteam.shop.dto.cart;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record CartItemRemoveRequestDto(
+        @NotNull(message = "Phone ID must be not null")
         Long phoneId,
+        @NotNull(message = "Amount must be not null")
+        @Min(value = 1, message = "Amount must be at least {value}")
+        @Max(value = 20, message = "Amount must not be greater than {value}")
         Integer amount
 ) {
 }
