39e5ebf5795dd2a3c0a028a3e1a84562691d9ffc
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2026-04-21 17:09:11 +0300

    refactor: move SecurityConfig into config.security package

--- src/main/java/com/challengeteam/shop/config/security/SecurityConfig.java
+++ src/main/java/com/challengeteam/shop/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.challengeteam.shop.config;
+package com.challengeteam.shop.config.security;
 
 import com.challengeteam.shop.properties.CorsProperties;
 import com.challengeteam.shop.security.filter.JwtTokenFilter;
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
