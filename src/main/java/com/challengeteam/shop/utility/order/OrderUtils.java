c503c34b6716c4e2de4d6c7f5d84b80f295b3e5b
Ihor Murashko <i.murashko0911@gmail.com>
2026-05-21 22:31:44 +0200

    correct imports

--- src/main/java/com/challengeteam/shop/utility/order/OrderUtils.java
+++ src/main/java/com/challengeteam/shop/utility/order/OrderUtils.java
@@ -73,8 +73,9 @@ public class OrderUtils {
      * Decreases the phone's stock by the ordered quantity and updates the product status
      * based on the remaining stock:
      * <ul>
-     *   <li>If remaining stock is 0, sets status to {@link ProductStatus#OUT_OF_STOCK}</li>
-     *   <li>If remaining stock is 5 or less, sets status to {@link ProductStatus#LOW_STOCK}</li>
+     *   <li>If remaining stock is 0, sets status to OUT_OF_STOCK</li>
+     *   <li>If remaining stock is 1-9, sets status to LOW_STOCK</li>
+     *   <li>If remaining stock is 10 or more, sets status to IN_STOCK</li>
      * </ul>
      * </p>
      *
@@ -84,13 +85,7 @@ public class OrderUtils {
     public static void updatePhoneStock(Phone phone, int orderedQuantity) {
         int remainingStock = phone.getStock() - orderedQuantity;
         phone.setStock(remainingStock);
+        phone.setStatus(ProductStatusResolver.resolve(remainingStock));
         log.info("Updated phone stock for phone: {} with remaining stock: {}", phone.getId(), remainingStock);
-        if (remainingStock == 0) {
-            log.info("Phone {} is out of stock", phone.getId());
-            phone.setStatus(ProductStatus.OUT_OF_STOCK);
-        } else if (remainingStock <= 5) {
-            log.info("Phone {} is low on stock", phone.getId());
-            phone.setStatus(ProductStatus.LOW_STOCK);
-        }
     }
 }
