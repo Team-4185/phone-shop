46c515d910319e93fd30b7e9ea65668e61160613
Ihor Murashko <i.murashko0911@gmail.com>
2026-05-27 15:37:12 +0200

    add color and storage check in OrderUtils for order creation

--- src/main/java/com/challengeteam/shop/utility/order/OrderUtils.java
+++ src/main/java/com/challengeteam/shop/utility/order/OrderUtils.java
@@ -2,7 +2,9 @@ package com.challengeteam.shop.utility.order;
 
 import com.challengeteam.shop.dto.order.request.item.OrderItemRequestDto;
 import com.challengeteam.shop.entity.phone.Phone;
+import com.challengeteam.shop.entity.phone.PhoneColor;
 import com.challengeteam.shop.entity.phone.ProductStatus;
+import com.challengeteam.shop.entity.phone.StorageCapacity;
 import com.challengeteam.shop.exceptionHandling.exception.order.OrderCreationException;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Utility class providing helper methods for order processing operations.
@@ -89,6 +92,26 @@ public class OrderUtils {
         } else if (remainingStock <= 5) {
             log.info("Phone {} is low on stock", phone.getId());
             phone.setStatus(ProductStatus.LOW_STOCK);
+        }
+    }
+
+    public static void checkIfColorAndStorageAvailable(
+            Map<Long, Phone> phoneMap, List<OrderItemRequestDto> items) {
+        for (OrderItemRequestDto item : items) {
+            Phone phone = phoneMap.get(item.phoneId());
+            Set<PhoneColor> availableColors = phone.getPhoneCharacteristics().getPhoneColors();
+            Set<StorageCapacity> availableStorages = phone.getPhoneCharacteristics().getStorageCapacities();
+
+            if (!availableColors.contains(item.color())) {
+                log.error("Color {} is not available for phone: {}", item.color(), phone.getName());
+                throw new OrderCreationException(
+                        "Color " + item.color() + " is not available for phone: " + phone.getName());
+            }
+            if (!availableStorages.contains(item.storage())) {
+                log.error("Storage {} is not available for phone: {}", item.storage(), phone.getName());
+                throw new OrderCreationException(
+                        "Storage " + item.storage() + " is not available for phone: " + phone.getName());
+            }
         }
     }
 }
