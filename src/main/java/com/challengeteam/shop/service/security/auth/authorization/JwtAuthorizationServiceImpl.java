b065b2ec702b4d9e5cbe9e7ce9b24245184da074
Ihor Murashko <i.murashko0911@gmail.com>
2026-06-02 03:43:46 +0200

    optimize imports

--- src/main/java/com/challengeteam/shop/service/security/auth/authorization/JwtAuthorizationServiceImpl.java
+++ src/main/java/com/challengeteam/shop/service/security/auth/authorization/JwtAuthorizationServiceImpl.java
@@ -3,7 +3,7 @@ package com.challengeteam.shop.service.security.auth.authorization;
 import com.challengeteam.shop.dto.auth.UserLoginRequestDto;
 import com.challengeteam.shop.dto.auth.UserRegisterRequestDto;
 import com.challengeteam.shop.dto.security.jwt.JwtResponseDto;
-import com.challengeteam.shop.dto.user.CreateUserDto;
+import com.challengeteam.shop.dto.user.request.CreateUserDto;
 import com.challengeteam.shop.entity.user.User;
 import com.challengeteam.shop.exceptionHandling.exception.CriticalSystemException;
 import com.challengeteam.shop.exceptionHandling.exception.InvalidAPIRequestException;
