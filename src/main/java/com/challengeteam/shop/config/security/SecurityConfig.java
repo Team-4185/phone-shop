709281c31f2dffcf08bcb985cdb8783e3a5e83df
Ihor Murashko <i.murashko0911@gmail.com>
2026-06-02 03:41:37 +0200

    add security config to restrict access

--- src/main/java/com/challengeteam/shop/config/security/SecurityConfig.java
+++ src/main/java/com/challengeteam/shop/config/security/SecurityConfig.java
@@ -72,6 +72,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/filter/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/orders").permitAll()
                         .requestMatchers("/api/v1/orders/**").authenticated()
+                        .requestMatchers("/api/v1/users/me").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
