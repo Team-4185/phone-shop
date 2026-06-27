b981e345121d3efa8f384de1188ecfaf6506eccc
Mykola Yefremov <efremovnikolaus@gmail.com>
2026-06-27 16:54:06 +0200

    style(payment): remove webhook dto trailing whitespace

--- src/main/java/com/challengeteam/shop/dto/payment/PaymentWebhookRequestDto.java
+++ src/main/java/com/challengeteam/shop/dto/payment/PaymentWebhookRequestDto.java
@@ -9,13 +9,13 @@ import java.io.Serializable;
 public record PaymentWebhookRequestDto(
         @NotBlank(message = "eventId is required")
         String eventId,
-        
+
         @NotBlank(message = "transactionId is required")
         String transactionId,
-        
+
         @NotNull(message = "paymentStatus is required")
         PaymentStatus paymentStatus,
-        
+
         String errorMessage
 ) implements Serializable {
 }
